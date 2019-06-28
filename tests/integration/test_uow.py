bc2aaaa386366b7b8bbbe019aa874dd445782ceb
Harry <hjwp2@cantab.net>
2019-06-28 16:07:25 +0100

    update tests

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -27,17 +27,24 @@ def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
     insert_batch(session, 'batch1', 'sku1', 100, None)
     session.commit()
 
-    uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
-    with uow:
-        batch = uow.batches.get(reference='batch1')
-        line = model.OrderLine('o1', 'sku1', 10)
-        batch.allocate(line)
-        uow.commit()
+    pytest.fail('decide what your UoW looks like first?')
+    # either:
+    # uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
+    # with uow:
+
+    # or perhaps
+    # with unit_of_work.start(session_factory) as uow: ?
+
+    #     batch = uow.batches.get(reference='batch1')
+    #     line = model.OrderLine('o1', 'sku1', 10)
+    #     batch.allocate(line)
+    #     uow.commit()
 
     batchref = get_allocated_batch_ref(session, 'o1', 'sku1')
     assert batchref == 'batch1'
 
 
+'''
 def test_rolls_back_uncommitted_work_by_default(session_factory):
     uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
     with uow:
@@ -61,3 +68,4 @@ def test_rolls_back_on_error(session_factory):
     new_session = session_factory()
     rows = list(new_session.execute('SELECT * FROM "batches"'))
     assert rows == []
+'''
